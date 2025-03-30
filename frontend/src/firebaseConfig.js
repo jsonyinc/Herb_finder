68f0bd1fdbe376c7065d45addbe7724e486c1604
Jason <jsonyinc@gmail.com>
2025-03-30 11:38:22 +0900

    Modify: apiKey 이슈

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -12,16 +12,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDXA-uxZnfynKOBck_J6-9o1Fs5uvVp9Pg",
-//     authDomain: "elite-cascade-452009-h3.firebaseapp.com",
-//     projectId: "elite-cascade-452009-h3",
-//     storageBucket: "elite-cascade-452009-h3.firebasestorage.app",
-//     messagingSenderId: "1050988738801",
-//     appId: "1:1050988738801:web:65c1add0adc4e1c8865150",
-//     measurementId: "G-QNMGKSHPV6"
-//   };
+}
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
